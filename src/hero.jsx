8077b23be782d6dfec15ced526a209b0326bd7a5
tabrani <129125220+Smeagol03@users.noreply.github.com>
2025-11-27 13:15:41 +0800

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "motion/react";
-// import { openInvitation } from "./komponen/open"
+import { openInvitation } from "./komponen/open"
 
 const Hero = () => {
   const container = {
